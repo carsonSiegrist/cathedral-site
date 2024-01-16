39dc6841cdfa5c741ca8de51106ece45efee0af0
carsonSiegrist <carsonsiegrist@gmail.com>
2024-01-16 01:10:22 -0700

    Added /start_game route, modified existing routes

--- flaskr/app.py
+++ flaskr/app.py
@@ -3,6 +3,7 @@ from game import Game
 
 
 app = Flask(__name__, static_url_path='/static')
+game = None #Holds board object
 
 @app.route('/')
 def home():
@@ -15,34 +16,29 @@ def home():
 def start_game():
     game = Game()
     game.start_game()
-    return game.get_board()
+    board = game.get_board()
+    board_dict = {"board" : str(board)}
+    return jsonify(board_dict)
 
 #API for getting the current board
 @app.route('/get_board', methods = ['GET'])
 def get_board():
-    #create a board object
-    #return board state as .json 
-    return jsonify([
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
-    [0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
-    [0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
-    ])
+    if not game:
+        raise RuntimeError("Game not started. Call /start_game first.")
+    board = game.get_board()
+    board_dict = {"board" : board}
+    return jsonify(board_dict)
 
 #API for playing a move
 #Returns current board state
 @app.route('/play_move', methods=['POST'])
 def play_move():
+    if not game:
+        raise RuntimeError("Game not started. Call /start_game first.")
     #Take user input from the request 
     move_data = request.get_json()
-    Game.play_move(move_data)
-    pass
+    game.play_move(move_data)
+    return
 
 if __name__ == '__main__':
     app.run(debug=True)
